f2a2ff3ef42c9eca2a769448835f1ad3a670e5d2
Dean Ralph <54666004+scootiemagee@users.noreply.github.com>
2020-02-11 12:14:28 +0000

    Updated login

--- main.py
+++ main.py
@@ -15,22 +15,16 @@ login = False
 
 @app.route('/', methods = ['POST', 'GET'])
 def index():
+    if request.method == 'POST':
+        login = request.form
+        with open('config.txt', 'w') as outfile:
+            outfile.write(login)
+        return "Settings Saved"
     if request.method == 'GET':
         if login:
             return render_template("index.html")
         else:
             return render_template("login.html")
-
-# @app.route('/settings', methods = ['POST', 'GET'])
-# def dbsettings():
-#     if request.method == 'POST':
-#         dbresult = request.form
-#         with open('dbconfig.json', 'w') as outfile:
-#             json.dump(dbresult, outfile, indent=2)
-#         return "Settings Saved"
-#     if request.method == 'GET':
-#         sqlConf = loadConfig('dbconfig.json')
-#         return render_template('settings.html', servervalue=sqlConf["server"], dbvalue=sqlConf["database"], uservalue=sqlConf["username"])
 
 if __name__ == "__main__":
     app.run()
