faddebc05901cbe50e3f96d113c85dd603c7095c
Dean Ralph <54666004+deanralph@users.noreply.github.com>
2020-02-11 09:33:36 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+# NESNAS v1
+# Dean Ralph
+# Jan 2020
+
+# Description:
+# Basic NAS interface with a NES theme
+
+#Imports
+from flask import Flask, render_template, request
+
+#Main App
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+# @app.route('/settings', methods = ['POST', 'GET'])
+# def dbsettings():
+#     if request.method == 'POST':
+#         dbresult = request.form
+#         with open('dbconfig.json', 'w') as outfile:
+#             json.dump(dbresult, outfile, indent=2)
+#         return "Settings Saved"
+#     if request.method == 'GET':
+#         sqlConf = loadConfig('dbconfig.json')
+#         return render_template('settings.html', servervalue=sqlConf["server"], dbvalue=sqlConf["database"], uservalue=sqlConf["username"])
+
+if __name__ == "__main__":
+    app.run()
