f8e3ec6902a2b2c7058650eb5e2c681e1ee61756
Dean Ralph <54666004+deanralph@users.noreply.github.com>
2020-02-14 12:46:16 +0000

    Update drive.py

--- drive.py
+++ drive.py
@@ -1,11 +1,13 @@
 import os
 import json
 
+arrDrives = {"Drive1": "Toshiba", "Drive2": "Toshiba", "Drive3": "Toshiba", "Drive4": "Toshiba"}
+
 def getDrives()
-  pass
+  return arrDrives
   
 def checkSmart()
-  pass
+  return "Pass"
 
 def noOfDisks()
   return 4
