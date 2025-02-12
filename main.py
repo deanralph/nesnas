f1db265081dbf0bf2dd64dcf8220399d12061f90
Dean Ralph <54666004+deanralph@users.noreply.github.com>
2025-02-12 01:10:05 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,32 +1,53 @@
-# NESNAS v1
-# Dean Ralph
-# Jan 2020
+from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
-# Description:
-# Basic NAS interface with a NES theme
-
-#Imports
-from flask import Flask, render_template, request
-import drive
-
-#Main App
 app = Flask(__name__)
+app.secret_key = '1D0nTKn0wW4tH15Is!'
 
-login = False
-noOfDrives = drive.noOfDisks()
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
-@app.route('/', methods = ['POST', 'GET'])
-def index():
+# Dummy user data
+users = {'nesplayer': {'password': 'password123'}}
+
+class User(UserMixin):
+    def __init__(self, username):
+        self.id = username
+
+@login_manager.user_loader
+def load_user(user_id):
+    if user_id in users:
+        return User(user_id)
+    return None
+
+@app.route('/')
+def home():
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
     if request.method == 'POST':
-        login = request.form
-        with open('config.txt', 'w') as outfile:
-            outfile.write(login)
-        return "Settings Saved"
-    if request.method == 'GET':
-        if login:
-            return render_template("index.html")
+        username = request.form['username']
+        password = request.form['password']
+        if username in users and users[username]['password'] == password:
+            user = User(username)
+            login_user(user)
+            return redirect(url_for('dashboard'))
         else:
-            return render_template("login.html")
+            flash('Invalid credentials. Try again.', 'danger')
+    return render_template('login.html')
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html', username=current_user.id)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
